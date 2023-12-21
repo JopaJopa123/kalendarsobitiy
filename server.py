d3db2d2c7aadfc59631c28656f4eb487a7905736
JopaJopa123 <154503318+JopaJopa123@users.noreply.github.com>
2023-12-21 12:37:37 +0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask, render_template, redirect, request, abort, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
